4142086caf125451ff8d053120cf9de9204da0a8
Zahir Mahmud <z.mahmud@asthait.com>
2025-09-04 15:55:50 +0600

    Sidebar icons updated and pg packages updated

--- apps/studio/src/components/sidebar/GlobalSidebar.vue
+++ apps/studio/src/components/sidebar/GlobalSidebar.vue
@@ -7,7 +7,7 @@
       :class="{ active: activeItem === 'tables'}"
       title="Database"
     >
-      <span class="bk-database" />
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-database-icon lucide-database"><ellipse cx="12" cy="5" rx="9" ry="3"/><path d="M3 5V19A9 3 0 0 0 21 19V5"/><path d="M3 12A9 3 0 0 0 21 12"/></svg>
     </a>
     <a
       href=""
@@ -16,7 +16,7 @@
       :class="{ active: activeItem === 'queries'}"
       title="Saved Queries"
     >
-      <span class="material-icons">code</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-braces-icon lucide-braces"><path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1"/><path d="M16 21h1a2 2 0 0 0 2-2v-5c0-1.1.9-2 2-2a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2h-1"/></svg>
     </a>
     <a
       href=""
@@ -25,7 +25,7 @@
       :class="{ active: activeItem === 'history'}"
       title="Run History"
     >
-      <span class="material-icons">history</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-clock-icon lucide-file-clock"><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M16 22h2a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v3"/><path d="M8 14v2.2l1.6 1"/><circle cx="8" cy="16" r="6"/></svg>
     </a>
     <a
       href=""
@@ -34,7 +34,7 @@
       :class="{ active: activeItem === 'joins'}"
       title="Join Query Builder"
     >
-      <span class="material-icons">join_inner</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-combine-icon lucide-combine"><path d="M10 18H5a3 3 0 0 1-3-3v-1"/><path d="M14 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2"/><path d="M20 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2"/><path d="m7 21 3-3-3-3"/><rect x="14" y="14" width="8" height="8" rx="2"/><rect x="2" y="2" width="8" height="8" rx="2"/></svg>
     </a>
     <span class="expand" />
     <!-- <core-account-button v-if="$store.state.workspaceId > 0" /> -->
